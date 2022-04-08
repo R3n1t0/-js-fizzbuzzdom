60f7e272c9489bab858add2f40c0addced83f036
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-08 17:46:48 +0200

    else and else if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 const limit = 104;
+
 const dividFizz = 3
 const dividBuzz = 5
 const dividFizzBuzz = 15
@@ -7,18 +8,30 @@ const dividFizzBuzz = 15
 for (let i=1; i <= limit; i++){
   const boxRl = document.createElement('div');
   boxRl.className = 'box';
+  container.append(boxRl);
   boxRl.append(i);
-  container.append(boxRl)
 
-  if(!(i % dividFizz)){
+  if(!(i % dividFizz) && (i % dividBuzz)){
     boxRl.classList.add("box-fizz");
+    const fizz = ("fizz");
+
+    boxRl.append(fizz)
+
+  }else if(!(i % dividBuzz) && (i % dividFizzBuzz)){
+    boxRl.classList.add("box-buzz");
+    
+    const buzz = ("buzz");
+    boxRl.append(buzz);
+
+
+  }else if(!(i % dividFizzBuzz)){
+    boxRl.classList.remove("box-fizz");
+    boxRl.classList.remove("box-buzz");
+    boxRl.classList.add("box-fizz-buzz");
+
+    console.log("boxRl");
+    const fizzBuzz = ("fizzbuzz");
+    boxRl.append(fizzBuzz);
   }
   
-  if(!(i % dividBuzz)){
-    boxRl.classList.add("box-buzz");
-  }
-
-  if(!(i % dividFizzBuzz)){
-    boxRl.classList.add("box-fizz-buzz");
-  }
 }
