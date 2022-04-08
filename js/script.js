b09fbf69e26bbaf16f1172dfcfd2e6b78060b6bf
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-08 17:58:09 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -9,29 +9,28 @@ for (let i=1; i <= limit; i++){
   const boxRl = document.createElement('div');
   boxRl.className = 'box';
   container.append(boxRl);
-  boxRl.append(i);
+  
 
   if(!(i % dividFizz) && (i % dividBuzz)){
     boxRl.classList.add("box-fizz");
-    const fizz = ("fizz");
 
-    boxRl.append(fizz)
+    let fizzBuzzDom = ("fizz");
+    boxRl.append(fizzBuzzDom)
 
   }else if(!(i % dividBuzz) && (i % dividFizzBuzz)){
     boxRl.classList.add("box-buzz");
     
-    const buzz = ("buzz");
-    boxRl.append(buzz);
+    let fizzBuzzDom = ("buzz");
+    boxRl.append(fizzBuzzDom);
 
 
   }else if(!(i % dividFizzBuzz)){
-    boxRl.classList.remove("box-fizz");
-    boxRl.classList.remove("box-buzz");
     boxRl.classList.add("box-fizz-buzz");
 
-    console.log("boxRl");
-    const fizzBuzz = ("fizzbuzz");
-    boxRl.append(fizzBuzz);
+    let fizzBuzzDom = ("fizzbuzz");
+    boxRl.append(fizzBuzzDom);
+  }else{
+    boxRl.append(i);
   }
   
 }
