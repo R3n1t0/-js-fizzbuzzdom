f1a094d9ee3f875a6cf94ca70138d9a4305541cf
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-08 16:07:18 +0200

    first script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 const container = document.querySelector(".container");
+const limit = 104;
+const dividFizz = 3
+const dividBuzz = 5
+const dividFizzBuzz = 15
+
+for (let i=1; i <= limit; i++){
+  const boxRl = document.createElement('div');
+  boxRl.className = 'box';
+  boxRl.append(i);
+  container.append(boxRl)
+
+  if(!(i % dividFizz)){
+    boxRl.classList.add("box-fizz");
+  }
+  
+  if(!(i % dividBuzz)){
+    boxRl.classList.add("box-buzz");
+  }
+
+  if(!(i % dividFizzBuzz)){
+    boxRl.classList.add("box-fizz-buzz");
+  }
+}
