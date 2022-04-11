3c5f06cd75772e35f24a26f41df5eb722a202e49
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-11 14:58:56 +0200

    corretto

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
 const container = document.querySelector(".container");
 const limit = 104;
 
-const dividFizz = 3
-const dividBuzz = 5
-const dividFizzBuzz = 15
-
 for (let i=1; i <= limit; i++){
   const boxRl = document.createElement('div');
   boxRl.className = 'box';
   container.append(boxRl);
+  let fizzBuzzDom = "";
   
-  if(!(i % dividFizz) && (i % dividBuzz)){
-    boxRl.classList.add("box-fizz");
-    let fizzBuzzDom = "fizz";
-    boxRl.append(fizzBuzzDom)
-
-  }else if(!(i % dividBuzz) && (i % dividFizzBuzz)){
-    boxRl.classList.add("box-buzz");
-    let fizzBuzzDom = "buzz";
-    boxRl.append(fizzBuzzDom)
-
-
-  }else if(!(i % dividFizzBuzz)){
+  
+  if(!(i % 15)){
     boxRl.classList.add("box-fizz-buzz");
     let fizzBuzzDom = "fizzbuzz";
     boxRl.append(fizzBuzzDom)
-
+    
+  }else if(!(i % 3)){
+    boxRl.classList.add("box-fizz");
+    let fizzBuzzDom = "fizz";
+    boxRl.append(fizzBuzzDom)
+    
+  }else if(!(i % 5)){
+    boxRl.classList.add("box-buzz");
+    let fizzBuzzDom = "buzz";
+    boxRl.append(fizzBuzzDom)
+    
   }else{
     boxRl.append(i)
   }
